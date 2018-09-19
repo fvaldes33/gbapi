d2e57490f16e3e402cdc3b41a891751e2446f693
Franco Valdes <franco.valdes89@gmail.com>
2018-09-19 10:23:42 -0400

    module & main

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,2 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var gbapi_1 = require("./gbapi");
-exports.default = gbapi_1.GBApi;
-var gbapi_2 = require("./gbapi");
-exports.County = gbapi_2.County;
-exports.GBApi = gbapi_2.GBApi;
-exports.State = gbapi_2.State;
-exports.Zip = gbapi_2.Zip;
+module.exports = require('./dist/ts-output.js').default;
---
+++ dist/ts-output.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var gbapi_1 = require("./gbapi");
+exports.GBApi = gbapi_1.GBApi;
+exports.default = gbapi_1.GBApi;
